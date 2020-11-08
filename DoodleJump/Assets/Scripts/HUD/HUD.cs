7711c470278b772e0832169a27c4d470c8535a5d
Praveen Liyanaarachchi <hello@praveen.one>
2020-11-08 16:57:50 +0900

    Game HUD

---
+++ DoodleJump/Assets/Scripts/HUD/HUD.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+    [SerializeField] LifeMeeter m_LifeMeeter;
+    [SerializeField] Text m_Coins;
+    [SerializeField] Text m_Score;
+    [SerializeField] GameObject m_HighScore;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InItPaintHUD();
+    }
+
+    void InItPaintHUD()
+    {
+        int lives = GameManager.Instance.GetLifeCount();
+        m_LifeMeeter.SetCount(lives);
+        m_Coins.text = GameManager.Instance.GetCoinCount().ToString();
+        m_Score.text = "0";
+    }
+}
